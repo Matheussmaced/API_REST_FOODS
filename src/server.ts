0f03e2059c167411ce7097cad2e89231de5a3c01
Matheussmaced <mateusbyakuya3@gmail.com>
2023-11-27 20:19:29 -0300

    feat: creating database and first migration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/users', () => {
-  return 'Users'
+app.get('/users', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
