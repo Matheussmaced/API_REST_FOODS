da6087c282a51331eb34c2bc199e1005d7720beb
Matheussmaced <mateusbyakuya3@gmail.com>
2023-11-29 20:51:43 -0300

    fix: resolved problem of import

--- src/routes/meals/get-meals-of-user.ts
+++ src/routes/meals/get-meals-of-user.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { checkSessionIdExist } from '../../middlewares/check-session-id-exist'
-import { getMealsOfUserUseCase } from '../../useCase/user/get-meals-of-user.use-case'
+import { getMealsOfUserUseCase } from '../../useCase/meals/get-meals-of-user.use-case'
 
 export async function getMealsOfUserRoutes(app: FastifyInstance) {
   app.get('/users/meals', { preHandler: [checkSessionIdExist] }, async () => {
--- src/useCase/meals/get-meals-of-user.use-case.ts
+++ src/useCase/meals/get-meals-of-user.use-case.ts
@@ -1,5 +1,5 @@
 import { knex } from '../../database'
-import { GetMealsOfUserDto } from '../../dto/user/get-meals-of-user.dto'
+import { GetMealsOfUserDto } from '../../dto/meals/get-meals-of-user.dto'
 
 export const getMealsOfUserUseCase = async (): Promise<GetMealsOfUserDto[]> => {
   const mealsUser = await knex('meals').select('*')
