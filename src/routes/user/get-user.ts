a31c8917944f32befcc32e0cc4a94297f7d86544
Matheussmaced <mateusbyakuya3@gmail.com>
2023-11-29 11:52:28 -0300

    feat: checking user by id

--- src/routes/user/get-user.ts
+++ src/routes/user/get-user.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { getUserUseCase } from '../../useCase/user/get-user.use-case'
+import { checkSessionIdExist } from '../../middlewares/check-session-id-exist'
 
 export async function getUsersRoutes(app: FastifyInstance) {
-  app.get('/users', async () => {
+  app.get('/users', { preHandler: [checkSessionIdExist] }, async () => {
     const users = await getUserUseCase()
 
     return users
--- src/routes/user/post-user.ts
+++ src/routes/user/post-user.ts
@@ -3,26 +3,31 @@ import { FastifyInstance } from 'fastify'
 import { PostUserDto } from '../../dto/user/post-user.dto'
 import { postUserUseCase } from '../../useCase/user/post-user.use-case'
 import { randomUUID } from 'crypto'
+import { checkSessionIdExist } from '../../middlewares/check-session-id-exist'
 
 export async function postUsersRoutes(app: FastifyInstance) {
-  app.post<{ Body: PostUserDto }>('/users', async (req, reply) => {
-    let sessionId = req.cookies.sessionId
+  app.post<{ Body: PostUserDto }>(
+    '/users',
+    { preHandler: [checkSessionIdExist] },
+    async (req, reply) => {
+      let sessionId = req.cookies.sessionId
 
-    if (!sessionId) {
-      sessionId = randomUUID()
+      if (!sessionId) {
+        sessionId = randomUUID()
 
-      reply.cookie('sessionId', sessionId, {
-        path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-      })
-    }
+        reply.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+        })
+      }
 
-    try {
-      await postUserUseCase(req.body, sessionId)
+      try {
+        await postUserUseCase(req.body, sessionId)
 
-      return reply.status(201).send()
-    } catch (error) {
-      return reply.status(500).send()
-    }
-  })
+        return reply.status(201).send()
+      } catch (error) {
+        return reply.status(500).send()
+      }
+    },
+  )
 }
